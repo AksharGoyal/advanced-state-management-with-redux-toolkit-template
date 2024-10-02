a26c9f51721202a8bdd8055857bd42e30e977ccd
Akshar Goyal <claim.akshar@gmail.com>
2024-10-02 15:43:00 +0530

    Added files

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -16,17 +16,17 @@ app.use(morgan("dev"));
 app.use(cookieParser(config.TOKEN));
 
 const corsOptions = {
-	// Add your address here i.e. your forwarded address from a cloud environment
-	origin: [
-		"http://127.0.0.1:5173",
-		"http://127.0.0.1:4173",
-		"http://localhost:5173",
-		"http://localhost:4173",
-		"http://[::1]:4173",
-		"http://[::1]:5173",
-	],
-	credentials: true, //included credentials as true
-	preflightContinue: true,
+    // Add your address here i.e. your forwarded address from a cloud environment
+    origin: [
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:4173",
+        "http://localhost:5173",
+        "http://localhost:4173",
+        "http://[::1]:4173",
+        "https://.github.dev"
+    ],
+    credentials: true, //included credentials as true
+    preflightContinue: true,
 };
 
 app.use(cors(corsOptions));
